f70d4d3cfbf23c901a6fc1fad1598e0e0d01520e
DEEPIKA KALAIVANAN <129764897+DeepikaKALAIVANAN@users.noreply.github.com>
2024-08-19 11:25:16 +0530

    Time: 5 ms (17.53%), Space: 10.3 MB (7.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,15 +1,16 @@
+// since only 3 values are there we compare the 3 values and swap it
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int left = 0, right = nums.size() - 1, mid = 0;
         while (mid <= right) {
-            if (nums[mid] == 0) {
+            if (nums[mid] == 0) { // if mid is 0 the value is swapped to left
                 swap(nums[left], nums[mid]);
                 left++;
                 mid++;
-            } else if (nums[mid] == 1) {
+            } else if (nums[mid] == 1) { // since the middle value should be 1 if mid is 1 then value is moved to right
                 mid++;
-            } else {
+            } else { // if mid is 2 then the value with right is swapped because 2 should be in right
                 swap(nums[mid], nums[right]);
                 right--;
             }

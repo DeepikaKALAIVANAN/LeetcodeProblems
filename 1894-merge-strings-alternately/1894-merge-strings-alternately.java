a10ff00bdfed875fbf62b3cff12ad30d5fae4797
DEEPIKA KALAIVANAN <129764897+DeepikaKALAIVANAN@users.noreply.github.com>
2025-03-12 10:11:07 +0530

    Time: 1 ms (82.98%), Space: 41.8 MB (57.76%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -4,24 +4,24 @@ class Solution {
         int i = 0;
         int j = 0;
         boolean flag = true;
-        while(i<word1.length() && j<word2.length()){  // check if i and j are less than their word sizes
-            if(flag){   // flag = true
-            sb.append(word1.charAt(i));  //print word1 value where pointer is pointing
-            i++;  // increment the pointer of word1
-        }else{   // if flag is false 
-            sb.append(word2.charAt(j)); // print the word2 value where pointer is pointing
-            j++;  // increment the pointer of word2
+        while(i < word1.length() && j < word2.length()){
+            if(flag){
+                sb.append(word1.charAt(i));
+                i++;
+            }else{
+                sb.append(word2.charAt(j));
+                j++;
+            }
+            flag = !flag;
         }
-        flag = !flag;  // toggle the boolean value of flag to false
-    }
-    while(i<word1.length()){
-        sb.append(word1.charAt(i));  // print all the remaining letters fron word1 after word2 ends 
-        i++;  // increment i
-    }
-    while(j<word2.length()){
-        sb.append(word2.charAt(j)); // print the remaining letters from word2 after word1 ends
-        j++; // increment j 
-    }
-    return sb.toString(); // convert stringbuilder to string and return the answer
+        while(i < word1.length()){
+            sb.append(word1.charAt(i));
+            i++;
+        }
+        while(j < word2.length()){
+            sb.append(word2.charAt(j));
+            j++;
+        }
+        return sb.toString();
     }
 }

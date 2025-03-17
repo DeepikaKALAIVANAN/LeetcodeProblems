101cea6e3d3759f0cb3f87bba4aa4680db86eac8
DEEPIKA KALAIVANAN <129764897+DeepikaKALAIVANAN@users.noreply.github.com>
2025-03-17 14:56:33 +0530

    Time: 0 ms (100%), Space: 41.6 MB (8.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 0) return 0;
-        if(nums.length==1) return nums[0];
-        int[] dp = new int[nums.length];
-        dp[0]=nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        for(int i=2;i<nums.length;i++){
-            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
-        }
-        return dp[nums.length-1];
+
+        // -----------RECURSION METHOD--------------
+        int[] dp=new int[nums.length];
+        //int n=nums.length;
+        Arrays.fill(dp,-1);
+        return dfs(nums,nums.length-1,dp);
+       
+    }
+    public int dfs(int[] arr,int n,int[] dp){
+        if(n<0) return 0;
+        if(n==0) return arr[0];
+        if(dp[n]!=-1) return dp[n];
+            int la=dfs(arr,n-2,dp)+arr[n];
+            int ra=dfs(arr,n-1,dp)+0;
+
+            dp[n]= Math.max(la,ra);
+            return dp[n];
+        
     }
 }

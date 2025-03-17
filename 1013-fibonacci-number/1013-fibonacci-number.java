0b723efd935a2b5a8b13ab5d71c66931d8152e04
DEEPIKA KALAIVANAN <129764897+DeepikaKALAIVANAN@users.noreply.github.com>
2025-03-17 12:03:22 +0530

    Time: 0 ms (100%), Space: 40.7 MB (17.7%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fibo(int n, int[] dp){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(dp[n] != 0){
+        return dp[n];
+        }
+        return dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp); 
+    }
+    public int fib(int n) {
+        int[] dp = new int[n + 1];
+        return fibo(n,dp);
+    }
+}

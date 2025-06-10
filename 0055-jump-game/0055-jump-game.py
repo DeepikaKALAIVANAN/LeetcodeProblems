78231c0f91225317fc502b7faab89ff909936c9c
DEEPIKA KALAIVANAN <129764897+DeepikaKALAIVANAN@users.noreply.github.com>
2025-06-10 09:56:28 +0530

    Time: 52 ms (21.09%), Space: 18.8 MB (24.83%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -3,6 +3,6 @@ class Solution:
         i = 0
         reach = 0
         while i < len(nums) and i <= reach:
-            reach = max(reach, i + nums[i])
-            i += 1
+            reach = max(reach, i+nums[i])
+            i+=1
         return i == len(nums)

5c144c06794b10eae75bf922a9745cbd52ba23a1
DEEPIKA KALAIVANAN <129764897+DeepikaKALAIVANAN@users.noreply.github.com>
2025-06-23 10:10:44 +0530

    Time: 5 ms (56.01%), Space: 18.1 MB (28.81%) - LeetHub

--- 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.py
+++ 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.py
@@ -4,7 +4,6 @@ class Solution:
         for i in nums:
             if abs(i) < abs(closest):
                 closest = i
-
         if closest < 0 and abs(closest) in nums:
             return abs(closest)
         return closest

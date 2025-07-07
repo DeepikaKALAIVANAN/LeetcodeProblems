aab7e557a1fa54298b1b60732ac35b43b22db895
DEEPIKA KALAIVANAN <129764897+DeepikaKALAIVANAN@users.noreply.github.com>
2025-07-07 10:32:33 +0530

    Time: 17 ms (61.08%), Space: 18.2 MB (13.29%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,19 +1,9 @@
-class Solution(object):
-    def canConstruct(self, ransomNote, magazine):
-        """
-        :type ransomNote: str
-        :type magazine: str
-        :rtype: bool
-        """
-        dict={}
-        for char in magazine:
-            if char not in dict:
-                dict[char] = 1
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        hash = Counter(magazine)
+        for ch in ransomNote:
+            if(hash[ch] > 0):
+                hash[ch] -= 1
             else:
-                dict[char] += 1
-        for char in ransomNote:
-            if char in dict and dict[char] > 0:
-                dict[char] -= 1
-            else: 
                 return False
         return True
